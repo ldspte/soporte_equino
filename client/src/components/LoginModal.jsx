fed5dbc33144f47936f0fad709e8b0eca5e8ebee
ldspte <ldspte9807@gmail.com>
2026-01-07 09:13:56 -0500

    ingreso

--- client/src/components/LoginModal.jsx
+++ client/src/components/LoginModal.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Modal, Button, Form, Alert } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ForgotPasswordModal from './ForgotPasswordModal';
 import { useAuth } from './AuthProvider'
 
-const LoginModal = ({ isOpen, onClose, onOpenForgot  }) => {
+const LoginModal = ({ isOpen, onClose, onOpenForgot }) => {
     if (!isOpen) return null;
+    const navigate = useNavigate();
     const { login } = useAuth();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -33,10 +35,11 @@ const LoginModal = ({ isOpen, onClose, onOpenForgot  }) => {
             const data = await response.json();
             if (response.ok) {
                 setSuccess(true);
-                await localStorage.setItem('token', data.token);
-                await localStorage.setItem('veterinario', JSON.stringify(data));
+                localStorage.setItem('token', data.token);
+                localStorage.setItem('veterinario', JSON.stringify(data));
+                login(data.token);
                 setTimeout(() => {
-                    window.location.href = '/home';
+                    navigate('/home');
                 }, 1000);
             } else {
                 setError(data.message || 'Error al iniciar sesión');
@@ -54,49 +57,49 @@ const LoginModal = ({ isOpen, onClose, onOpenForgot  }) => {
 
     return (
         <>
-        <Modal show={isOpen} onHide={onClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>Iniciar Sesión</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                {error && <Alert variant="danger">{error}</Alert>}
-                {success && <Alert variant="success">¡Login exitoso! Redirigiendo...</Alert>}
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group controlId="username">
-                        <Form.Label>Usuario:</Form.Label>
-                        <Form.Control 
-                            type="text" 
-                            value={email} 
-                            required 
-                            onChange={(e) => setEmail(e.target.value)} 
-                        />
-                    </Form.Group>
-                    <Form.Group controlId="password">
-                        <Form.Label>Contraseña:</Form.Label>
-                        <Form.Control 
-                            type="password" 
-                            value={password} 
-                            onChange={(e) => setPassword(e.target.value)} 
-                            required 
-                        />
-                    </Form.Group>
-                    <Button variant="warning" type="submit" disabled={loading}>
-                        {loading ? 'Cargando...' : 'Iniciar Sesión'}
-                    </Button>
-                    <Button variant="secondary" onClick={onClose} className="ml-2">
-                        Cerrar
-                    </Button>
-                </Form>
-                <div className="text-center mt-3">
-                    <Button variant="link" onClick={handleOpenForgotModal}>
-                        ¿Olvidaste tu contraseña?
-                    </Button>
-                </div>
-            </Modal.Body>
-        </Modal>
-        <ForgotPasswordModal 
-                isOpen={showForgotModal} 
-                onClose={() => setShowForgotModal(false)} 
+            <Modal show={isOpen} onHide={onClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Iniciar Sesión</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    {success && <Alert variant="success">¡Login exitoso! Redirigiendo...</Alert>}
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId="username">
+                            <Form.Label>Usuario:</Form.Label>
+                            <Form.Control
+                                type="text"
+                                value={email}
+                                required
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="password">
+                            <Form.Label>Contraseña:</Form.Label>
+                            <Form.Control
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </Form.Group>
+                        <Button variant="warning" type="submit" disabled={loading}>
+                            {loading ? 'Cargando...' : 'Iniciar Sesión'}
+                        </Button>
+                        <Button variant="secondary" onClick={onClose} className="ml-2">
+                            Cerrar
+                        </Button>
+                    </Form>
+                    <div className="text-center mt-3">
+                        <Button variant="link" onClick={handleOpenForgotModal}>
+                            ¿Olvidaste tu contraseña?
+                        </Button>
+                    </div>
+                </Modal.Body>
+            </Modal>
+            <ForgotPasswordModal
+                isOpen={showForgotModal}
+                onClose={() => setShowForgotModal(false)}
             />
         </>
     );
